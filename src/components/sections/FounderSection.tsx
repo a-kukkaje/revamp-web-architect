5f774a1488c60b8511e813dec1969441b5bc3fcc
a-kukkaje <136228296+a-kukkaje@users.noreply.github.com>
2025-04-29 11:49:10 +0530

    Update FounderSection.tsx

--- src/components/sections/FounderSection.tsx
+++ src/components/sections/FounderSection.tsx
@@ -15,7 +15,7 @@ const FounderSection = () => {
         <div className="grid md:grid-cols-2 gap-12 items-center mb-16">
           <div>
             <img 
-              src="../../src/assets/Swami.png" 
+              src="../../assets/Swami.png" 
               alt="Swami" 
               className="rounded-lg shadow-lg"
             />
